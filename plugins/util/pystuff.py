0de45f41b37aa33e73034ecd4b827f0cd22966fd
Simone Esposito <webmaster@freebnc.net>
2012-05-01 17:15:07 +0200

    fix?

--- plugins/util/pystuff.py
+++ plugins/util/pystuff.py
@@ -1,6 +1,7 @@
 import http
 import re
 import sys
+from util import hook, http
 
 def warmup(code = "2+2", nick=None):
     preres = http.get("http://eval.appspot.com/eval",statement=code, nick=None)
