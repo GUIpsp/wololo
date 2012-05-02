02978f5842e0f1b87c07a7b7f0d37c76ba955776
Guilherme Espada <porcariadagata@gmail.com>
2012-05-02 21:20:24 +0200

    I'M JUST HUMAN OK

--- plugins/util/randout.py
+++ plugins/util/randout.py
@@ -6,9 +6,9 @@ if enable:
 	asuccess = ['\o/','\o','o/','8D','=D','（ ﾟーﾟ)','(〜￣▽￣)〜',':D','o7',':DDDDDDDDDDDDD']
 	afail = ['（ ´_ゝ｀）','(　ﾟ,_ゝ,ﾟ)','=(',':(',':<',':\'(','D:','DDD:','ಠ_ಠ']
 	def success():
-		return achoice(success)
+		return choice(asuccess)
 	def fail():
-		return achoice(fail)
+		return choice(afail)
 else:
 	def success():
 		return False
